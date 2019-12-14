3841201cd87d588a83c2bb21945a8220d3081f2b
fanfx <ffxbao@gmail.com>
2019-12-15 00:59:29 +0800

    finish

--- src/main/java/com/fanfx/hourcalculator/controller/CountAction.java
+++ src/main/java/com/fanfx/hourcalculator/controller/CountAction.java
@@ -1,15 +1,14 @@
 package com.fanfx.hourcalculator.controller;
 
-import com.fanfx.hourcalculator.Utils.MediaTypeUtils;
 import com.fanfx.hourcalculator.exception.SysCodeEnums;
 import com.fanfx.hourcalculator.service.impl.CountServiceImpl;
 import com.fanfx.hourcalculator.vo.DataResult;
 import com.fanfx.hourcalculator.vo.UploadVo;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.util.ResourceUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,32 +23,32 @@ import java.io.*;
 public class CountAction {
     private ServletContext servletContext;
     private CountServiceImpl countService;
+
     @Autowired
-    public CountAction(ServletContext servletContext,CountServiceImpl countService) {
+    public CountAction(ServletContext servletContext, CountServiceImpl countService) {
         this.servletContext = servletContext;
         this.countService = countService;
     }
 
-//    private static Logger logger = LoggerFactory.getLogger(CountAction.class);
+    private static Logger logger = LoggerFactory.getLogger(CountAction.class);
 
     @SuppressWarnings("all")
     @PostMapping(value = "/count")
     public DataResult count(@Validated UploadVo vo) throws Exception {
         MultipartFile file = vo.getFile();
-        String tmpName = "temp"+file.getOriginalFilename();
+        String tmpName = "temp" + file.getOriginalFilename();
         File tempFile = new File(tmpName);
         BufferedOutputStream bo = new BufferedOutputStream(new FileOutputStream(new File(tmpName)));
         BufferedInputStream bs = new BufferedInputStream(file.getInputStream());
         int r;
-        while ((r = bs.read())!=-1){
+        while ((r = bs.read()) != -1) {
             bo.write(r);
         }
         bo.flush();
         bo.close();
 
-
-        String fileName = "calculated_"+file.getOriginalFilename();
-        Workbook workbook = countService.count(tempFile,file);
+        String fileName = "calculated_" + file.getOriginalFilename();
+        Workbook workbook = countService.count(tempFile, file);
         File outFile = new File(fileName);
         FileOutputStream fs = new FileOutputStream(outFile);
         workbook.write(fs);
@@ -61,17 +60,20 @@ public class CountAction {
 
     @GetMapping(value = "download")
     public String download(HttpServletResponse response) throws Exception {
-        String path = ResourceUtils.getURL("classpath:").getPath()+"static/";
-        File d = new File(path);
-        File[] files = d.listFiles();
-        assert files != null;
-        File file = files[0];
-        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(this.servletContext, file.getName());
-        response.setContentType(mediaType.toString());
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName());
-        response.setContentLength((int) file.length());
-        try(BufferedInputStream inStream = new BufferedInputStream(new FileInputStream(file))){
-            try(BufferedOutputStream outStream = new BufferedOutputStream(response.getOutputStream())){
+//        MediaType mediaType =
+//                MediaTypeUtils.getMediaTypeForFileName(this.servletContext, "demo.xls");
+
+        InputStream resourceAsStream =
+                getClass().getClassLoader().getResourceAsStream("static/demo.xls");
+
+//        response.setContentType(mediaType.toString());
+        response.setContentType("application/octet-stream");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=demo.xls");
+        response.setContentLength(resourceAsStream.available());
+
+        try (BufferedInputStream inStream = new BufferedInputStream(resourceAsStream)) {
+            try (BufferedOutputStream outStream =
+                    new BufferedOutputStream(response.getOutputStream())) {
                 byte[] buffer = new byte[1024];
                 int bytesRead;
                 while ((bytesRead = inStream.read(buffer)) != -1) {
@@ -82,5 +84,4 @@ public class CountAction {
         }
         return null;
     }
-
 }
